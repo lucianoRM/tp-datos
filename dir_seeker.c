b6a1472798350c15fa2f64308fb0f2e6ab8158c1
Luciano <luciano.rmna@gmail.com>
2013-10-16 00:03:29 -0300

    Agrego dir_seeker.c. Lo que hace la funcion que tiene adentro es recorrer un directorio(Tiene hardcodeado: 'Libros', asi que se deberia nombrar de la misma manera para que funcione) e imprimir los nombres de todos los archivos dentro del directorio

---
+++ dir_seeker.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <stdlib.h>
+
+/*Funcion que recorre un directorio e imprime los nombres de los archivos que se encuentran dentro de el*/
+void imprimir_nombre_archivos(char* nombre_dir){
+	DIR* dir_pointer = opendir(nombre_dir);
+	struct dirent* reg_buffer = readdir(dir_pointer);
+	while(reg_buffer != NULL){
+		printf("%s\n",reg_buffer->d_name);
+		reg_buffer = readdir(dir_pointer);
+	}
+	closedir(dir_pointer);
+}
+
+int main(){
+	imprimir_nombre_archivos("Libros");
+	return 0;
+}
