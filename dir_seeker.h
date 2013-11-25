b1bd3ef19d84cb48a0a675dc49b4a02ac430129c
Luciano <luciano.rmna@gmail.com>
2013-11-25 12:49:04 -0300

    Ahora si agrego main que no estaba

---
+++ dir_seeker.h
@@ -0,0 +1,26 @@
+#ifndef DIR_SEEKER_H
+#define DIR_SEEKER_H
+
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <stdlib.h>
+#include <string>
+#include "parser/parser.h"
+#include <dirent.h>
+#include <string.h>
+#include <time.h>
+#include "porter/porter.h"
+#include "parser/vectorizador.h"
+
+
+
+
+/*Carga todo lo que se guarda en los hashes en disco*/
+void guardar_terminos(map<string,unsigned int>* hash_frecuencias_globales,map<string,map<string,unsigned int> >* hash_frecuencias_locales);
+
+
+/*Funcion que recorre un directorio e imprime los nombres de los archivos que se encuentran dentro de el*/
+int parsear_archivos(string nombre_dir,Parser* parser);
+
+#endif //DIR_SEEKER_H
---
+++ main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <stdlib.h>
+#include <string>
+#include "parser/parser.h"
+#include <dirent.h>
+#include <string.h>
+#include <time.h>
+#include "porter/porter.h"
+#include "parser/vectorizador.h"
+#include "dir_seeker.h"
+
+#define NOMBRE_TP "./TpGrupo6"
+
+using std::map;
+using std::string;
+using std::ifstream;
+using std::cout;
+using std::endl;
+using std::ofstream;
+
+
+void help(){
+
+	cout << "Uso:\n\nCreacion de Clusters:\n\n" << NOMBRE_TP << " -d <path> -c <#clusters> -o <Y/N>\n\n-d indica el path a donde están almacenados los documentos.\n-c indica la cantidad de categorias a crear, si no se indica se decide automaticamente automaticamente.\n-o indica si un documento puede estar en mas de un grupo.\n\nListado de resultados:\n\n" << NOMBRE_TP	 << " -l\nLista todos los documentos del repositorio y la categoría a la cual pertenece cada uno.\n\n" << NOMBRE_TP << " -g\nLista los grupos o categorías existentes y los documentos dentro de cada grupo o categoría.\n\nAgregado de archivos:\n\n" << NOMBRE_TP << " -a <path>\nAgrega y clasifica el texto pasado como parametro e indica a que grupo lo ha agregado."; 
+
+}
+
+int main(int args,char* argv[]){
+	
+	if (args == 1){
+		help();
+		return 0;
+	}else{
+		if(strcmp(argv[1],"-d") == 0){
+			if(args < 3){
+				help();
+				return 1;
+			}
+			int t_inicio = time(NULL);
+			Parser* parser =new Parser();
+			int retorno = parsear_archivos(argv[2],parser);
+			if (retorno != 0){
+				cout << "No existe directorio\n";
+				delete parser;
+				return retorno;
+			}
+			cout << "Cantidad de archivos: "<< parser->getCantDocs() << endl;
+			cout << "Cantidad de terminos: "<< parser->getCantTerms() << endl;
+			int t_fin = time(NULL);
+			cout << "Tardo: " << t_fin - t_inicio << " segundos" << endl;
+			delete parser;
+		}
+	}
+	return 0;
+}
