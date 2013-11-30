9807e2b0eb603d7f6f3d3fb98a7b5867f2ce2ebc
Tincho <tincho@tincho-VirtualBox.(none)>
2013-11-30 18:30:35 -0300

    Subo el vectorizador optimizado que devuelve 30% de los vectores

--- parser/vectorizador.cpp
+++ parser/vectorizador.cpp
@@ -14,31 +14,15 @@ using std::ofstream;
 
 #define MAX 100
 #define PATH "vectores/"
-
-void cargar_a_disco(map<string,map<unsigned int,float> >* map_vectores){
-	map<string,map<unsigned int,float> >::iterator it_docs;
-	string nombre_archivo = PATH;
-	map<unsigned int,float>::iterator it_terms;
-	for(it_docs = map_vectores->begin(); it_docs != map_vectores->end(); ++it_docs){
-		nombre_archivo += it_docs->first;
-		ofstream archivo(nombre_archivo.c_str());
-		for (it_terms = it_docs->second.begin(); it_terms != it_docs->second.end(); ++it_terms)
-			archivo << it_terms->first << ":" << it_terms->second << endl;
-		archivo.close();
-		nombre_archivo = PATH;
-	}
-}
+#define PORCENTAJE_VECTORES 0.3		
 		
-		
-	
-
-
 
 map<string,map<unsigned int,float> >* vectorizar(Parser* parser){
 
 	unsigned int archivos_procesados = 0;
 	map<string,unsigned int> hash_referencias; //Guarda las referencias de los terminos a los int que los representan
 	map<string,map<unsigned int,float> >* vectores = new map<string,map<unsigned int,float> > ; //Vectores que representan a los archivos.
+	map<unsigned int, float> vector_actual;
 	map<string,float> pesos_globales; //Se guardan los pesos globales calculados;
 	map<string,unsigned int>::iterator it_global;
 	map<string,map<string,unsigned int> >::iterator it_local_ext;
@@ -47,31 +31,39 @@ map<string,map<unsigned int,float> >* vectorizar(Parser* parser){
 	float peso;
 	float cant_docs = (float)parser->getCantDocs();
 	float division;
+	unsigned int distancia_docs = (int)(1/ PORCENTAJE_VECTORES);
+	
 	for(it_global = parser->getFrecuenciasGlobales()->begin(); it_global !=  parser->getFrecuenciasGlobales()->end(); ++it_global){
 		division = (cant_docs/(float)it_global->second);
 		hash_referencias[it_global->first] = i;
 		pesos_globales[it_global->first] = 1 + log10(division);
 		i++;
 	}
-	
+	string nombre_archivo = PATH;
+	map<unsigned int,float>::iterator it_terms;
 	for(it_local_ext = parser->getFrecuenciasLocales()->begin(); it_local_ext != parser->getFrecuenciasLocales()->end(); ++it_local_ext){
 		cout << "Vectorizando: " << it_local_ext->first << endl;		
 		for(it_local_int = it_local_ext->second.begin();it_local_int != it_local_ext->second.end();++it_local_int){
 			peso = ((float)it_local_int->second) * pesos_globales[it_local_int->first];
-			(*vectores)[it_local_ext->first][hash_referencias[it_local_int->first]] = peso;
+			vector_actual[hash_referencias[it_local_int->first]] = peso;
 		}
+
+		if (archivos_procesados % distancia_docs == 0)
+			(*vectores)[it_local_ext->first] = vector_actual;
+		
+		  ///////////////
+		//Escritura en Disco
+		nombre_archivo += it_local_ext->first;
+		ofstream archivo(nombre_archivo.c_str());	
+
+		for (it_terms = vector_actual.begin(); it_terms != vector_actual.end(); ++it_terms)
+			archivo << it_terms->first << ":" << it_terms->second << endl;
+		
+		archivo.close();
+		////////////////
 		archivos_procesados++;
-		/*if(archivos_procesados > MAX){
-				cout << "WRITING" << endl;
-				cargar_a_disco(vectores);
-				delete vectores;
-				vectores = new map<string,map<unsigned int,float> >;
-				archivos_procesados = 0;
-		}*/
 	}
-	cargar_a_disco(vectores);
+	
 	return vectores;
 }
-
-	
 
